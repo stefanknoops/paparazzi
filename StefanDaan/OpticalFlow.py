c16eac39454c241428e62a4b862816e253ca7c90
Daan Cuppen <daan.cuppen@live.nl>
2020-03-04 10:51:54 -0500

    Optical Flow File

---
+++ StefanDaan/OpticalFlow.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import glob
+
+#load images
+images = [cv2.imread(file) for file in glob.glob("/home/daan/Documents/AE4317_2019_datasets/sim_poles/20190121-160844/*.jpg")]
+plt.imshow(images[4], cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
+
+#write video from images
+height,width,layers = images[0].shape
+size = (width,height)
+out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+
+for i in range(len(images)):
+    out.write(images[i])
+out.release()
+
+#Perform dense optical flow calculations
+cap = cv2.VideoCapture("project.avi")
+
+#define feature vector
+ret, frame1 = cap.read()
+prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+hsv = np.zeros_like(frame1)
+hsv[...,1] = 255
+
+while(1):
+    ret, frame2 = cap.read()
+    next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+
+    flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    pause
+    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+    hsv[...,0] = ang*180/np.pi/2
+    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+    rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+
+    cv2.imshow('frame2',rgb)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    elif k == ord('s'):
+        cv2.imwrite('opticalfb.png',frame2)
+        cv2.imwrite('opticalhsv.png',rgb)
+    prvs = next
+
+cap.release()
+cv2.destroyAllWindows()
