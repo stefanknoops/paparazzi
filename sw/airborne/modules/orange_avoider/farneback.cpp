b74e0cfe35b128bfd838214e15c4441b2a7a221d
rubenmeester <ruben.meester97@hotmail.com>
2020-03-19 11:50:34 +0100

    Changes farneback values 43 and 3 and inputarray current

--- sw/airborne/modules/orange_avoider/farneback.cpp
+++ sw/airborne/modules/orange_avoider/farneback.cpp
@@ -31,14 +31,14 @@ struct prev = ;//vorige afbeelding
 struct current = ;//huidige afbeelding
 double pyr_scale = 0.5; //image scale for pyramids <1
 int levels = 1; //number of pyramid layers (?) where 1 means that there are no extra layers
-int winsize = ;// window size
-int iterations = 1;
+int winsize = 43 ;// window size (43; source ruben)
+int iterations = 3; ;// (3; source ruben)
 int poly_n = 5; //size of the pixel neighborhood used to find polynomial expansion in each pixel
 double poly_sigma = 1.1 //standard deviation of the Gaussian that is used to smooth derivatives used as a basis for the polynomial expansion;
 
 // hier farneback doen
 // waardes van Daan voor nu gebruiken?
-void calcOpticalFlowFarneback(InputArray prev, InputArray next, InputOutputArray flow, double pyr_scale, int levels, int winsize, int iterations, int poly_n, double poly_sigma) {
+void calcOpticalFlowFarneback(InputArray prev, InputArray current, InputOutputArray flow, double pyr_scale, int levels, int winsize, int iterations, int poly_n, double poly_sigma) {
     flow_im = flow;
 }
 
