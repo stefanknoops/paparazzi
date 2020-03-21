569593ecaa55d465aec311b17900093f978a01cd
maxkoster <maxkoster@hotmail.com>
2020-03-21 12:12:22 +0100

    Voor daan

---
+++ sw/airborne/modules/testmaxtest/testmax.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) Max Koster
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/testmaxtest/testmax.c"
+ * @author Max Koster
+ * testing module creator
+ */
+
+#include "modules/testmaxtest/testmax.h"
+
+// void max_init {}
+
+
---
+++ sw/airborne/modules/testmaxtest/testmax.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) Max Koster
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/testmaxtest/testmax.h"
+ * @author Max Koster
+ * testing module creator
+ */
+
+#ifndef TESTMAX_H
+#define TESTMAX_H
+
+// extern void max_init;
+
+#endif
+
