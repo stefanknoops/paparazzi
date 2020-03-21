e993775a730499110b484514009510b1d0c7dcda
stefanknoops <stefan.knoops@gmail.com>
2020-03-21 16:28:38 +0100

    created farneback module

---
+++ sw/airborne/modules/farneback_avoider/farneback_avoider.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) Max_Stefan
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/farneback_avoider/farneback_avoider.h"
+ * @author Max_Stefan
+ * Adaption of orange avoider in order to use the Farneback method
+ */
+
+#ifndef FARNEBACK_AVOIDER_H
+#define FARNEBACK_AVOIDER_H
+
+// extern void farneback_init();
+// extern void farneback_periodic;
+
+#endif
+
