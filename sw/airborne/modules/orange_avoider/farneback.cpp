89d384f981dbdb8017baec4380f950b03111b92f
stefanknoops <stefan.knoops@gmail.com>
2020-03-21 12:27:43 +0100

    zodat max kan werken

--- sw/airborne/modules/orange_avoider/farneback.cpp
+++ sw/airborne/modules/orange_avoider/farneback.cpp
@@ -38,8 +38,14 @@ double poly_sigma = 1.1 //standard deviation of the Gaussian that is used to smo
 
 // hier farneback doen
 // waardes van Daan voor nu gebruiken?
-void calcOpticalFlowFarneback(InputArray prev, InputArray next, InputOutputArray flow, double pyr_scale, int levels, int winsize, int iterations, int poly_n, double poly_sigma) {
+struct image_t *farneback_func(struct image_t *img)
+{
+
+    flow = calcOpticalFlowFarneback(struct image_t prev, struct image_t current, InputOutputArray flow, double pyr_scale, int levels, int winsize, int iterations, int poly_n, double poly_sigma) 
     flow_im = flow;
+
+
+    prev = 
 }
 
 
