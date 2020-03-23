f4c9162c96cc2e03c077edf70a280927cb7382d7
stefanknoops <stefan.knoops@gmail.com>
2020-03-23 12:28:09 +0100

    added farneback module

---
+++ sw/airborne/modules/farneback-avoider/farneback-avoider.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) Max_Stefan
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/farneback_avoider/farneback_avoider.c"
+ * @author Max_Stefan
+ * Adaption of orange avoider in order to use the Farneback method
+ */
+
+#ifndef FARNEBACK_FPS
+#define FARNEBACK_FPS 0       ///< Default FPS (zero means run at camera fps)
+#endif
+PRINT_CONFIG_VAR(FARNEBACK_FPS)
+
+#include "modules/farneback_avoider/farneback_avoider.h"
+#include "modules/computer_vision/cv.h"
+#include "modules/orange_avoider/farneback.hpp" //staat nu wel op een gekke plek maar prima // we moeten deze wel nog maken
+#include "modules/computer_vision/opticflow/linear_flow_fit.h"
+#include "pthread.h"
+
+struct image_t *prev_img;
+float ttc;
+float ttc_temp;
+
+static pthread_mutex_t mutex;
+
+float error_threshold = 0.25;
+int n_iterations = 1;
+int n_samples = 3;
+int im_width = front_camera.output_size.w; //dit werkt wss niet
+int im_height = front_camera.output_size.h;
+struct linear_flow_fit_info *info;
+  
+bool new_ttc = false
+static float call_ttc(struct image_t *img) { //video callback function
+  struct flow_t *vectors = farneback_func(struct image_t *img,struct image_t *prev_img) 
+  //linear_flow_fit
+  if analyze_linear_flow_field(struct flow_t *vectors, int count, float error_threshold, int n_iterations, int n_samples, int im_width, int im_height, struct linear_flow_fit_info *info)
+  {
+  ttc_temp = info->time_to_contact;
+  new_ttc = true
+  }
+ 
+  
+  pthread_mutex_lock(&mutex);
+  ttc  =  ttc_temp;
+  pthread_mutex_unlock(&mutex);
+  
+  *prev_img = *img;
+  return ttc_temp //niet zeker maar voor nu
+}
+
+void farneback_init(struct image_t *img) {
+  //pas airframe aan in xml
+  cv_add_to_device(&FARNEBACK_CAMERA, call_ttc, FARNEBACK_FPS);
+  *prev_img = *img;
+  AbiBindMsgVISUAL_DETECTION(ORANGE_AVOIDER_VISUAL_DETECTION_ID, &color_detection_ev, color_detection_cb); //dit gebruiken om hem in orange avoider te implementen
+
+  memset(ttc, 0, sizeof(float));
+  pthread_mutex_init(&mutex, NULL);
+}
+
+
+
+void farneback_periodic()
+{
+   static struct color_object_t local_filters[2];
+  pthread_mutex_lock(&mutex);
+  memcpy(local_filters, global_filters, 2*sizeof(struct color_object_t));
+  pthread_mutex_unlock(&mutex);
+
+  //is deze if hier nodig? Zo ja moeten we de conditie ergens updaten
+  //de local filters moeten eruit want die gebruiken we niet
+  if(new_ttc){
+    AbiSendMsgVISUAL_DETECTION(FARNEBACK_OBJECT_DETECTION_ID, ttc);
+    new_ttc = false;
+  }
+}
+
+
---
+++ sw/airborne/modules/farneback-avoider/farneback.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "modules/computer_vision/cv.h"
+#include "modules/computer_vision/lib/vision/image.h"
+
+using namespace std;
+#include <opencv2/core/core.hpp>			//Hier staan de cart2polar en normalize in
+#include <opencv2/cudaoptflow/include/optflow.hpp>
+#include <opencv2/video/tracking.hpp>		//Hier staat de farneback methode in
+#include <opencv2/imgproc/imgproc.hpp>		//Hier staan de resize en cvtcolor in
+// hier moet de juiste opencv functie nog worden toegevoegd
+
+using namespace cv;
+
+#ifndef FARNEBACK_FPS 
+#define FARNEBACK_FPS 0       ///< Default FPS (zero means run at camera fps)
+#endif
+PRINT_CONFIG_VAR(FARNEBACK_FPS)
+
+#ifndef FARNEBACK_SEND_OBSTACLE
+#define FARNEBACK_SEND_OBSTACLE FALSE    ///< Default sonar/agl to use in opticflow visual_estimator
+#endif
+PRINT_CONFIG_VAR(FARNEBACK_SEND_OBSTACLE)
+
+struct video_listener *listener = NULL;
+
+// hier waardes initialiseren
+int fb_thresh = 10;
+int optical_flow = 0;
+
+// het vorige beeld en het huidige beeld moet worden ingeladen ergens (volgens mij dit)
+struct prev = ;//vorige afbeelding
+struct current = ;//huidige afbeelding
+double pyr_scale = 0.5; //image scale for pyramids <1
+int levels = 1; //number of pyramid layers (?) where 1 means that there are no extra layers
+int winsize = 43 ;// window size (43; source ruben)
+int iterations = 3; ;// (3; source ruben)
+int poly_n = 5; //size of the pixel neighborhood used to find polynomial expansion in each pixel
+double poly_sigma = 1.1 //standard deviation of the Gaussian that is used to smooth derivatives used as a basis for the polynomial expansion;
+
+// hier farneback doen
+// waardes van Daan voor nu gebruiken?
+struct flow_t *farneback_func(struct image_t *img)
+{
+
+    flow = calcOpticalFlowFarneback(struct image_t prev, struct image_t current, InputOutputArray flow, double pyr_scale, int levels, int winsize, int iterations, int poly_n, double poly_sigma) 
+    flow_im = flow;
+
+
+    prev = 
+    //de flow vector en het formaat daarvan (count) moet ergens worden opgeslagen
+    //wat hier moet gebeuren
+    //is zorgen dat de flow uitvoer in een formaat komt wat overeenkomt met het flow_t van paparazzi
+    //https://docs.opencv.org/2.4/modules/video/doc/motion_analysis_and_object_tracking.html
+    //http://docs.paparazziuav.org/v5.10/image_8h.html#structflow__t
+}
+
+
+// hier treshold checken
+
+
+
+// hier actie ondernemen?
